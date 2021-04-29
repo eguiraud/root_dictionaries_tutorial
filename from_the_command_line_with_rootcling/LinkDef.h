b26782be394cac80110872bccaaf24d5bf88de2b
Enrico Guiraud <enrico.guiraud@cern.ch>
2021-04-29 21:38:03 +0200

    [WIP] More docs about linkdef syntax

--- from_the_command_line_with_rootcling/LinkDef.h
+++ from_the_command_line_with_rootcling/LinkDef.h
@@ -16,3 +16,28 @@
 //#pragma link C++ class TwoInts-;
 
 #endif // __CLING__
+
+
+// MORE LinkDef syntax:
+// +: new streamer
+// nothing: old streamer (no schema evolution)
+// -: no streamer generated (User must write their own)
+// this applies to classes with a ClassDef.
+// without a ClassDef it is a no-op.
+// Philippe Canal
+// 16:29
+// 
+// a trailing '!' means "no input operator" (use to generator operator<< for TBuffer).
+// 
+// //  #pragma link [spec] options=... class classname[+-!]
+// 
+// (see core/dictgen/src/LinkdefReader.cxx()
+//       /*    options=...
+//        * possible options:
+//        *   nostreamer: set G__NOSTREAMER flag
+//        *   noinputoper: set G__NOINPUTOPERATOR flag
+//        *   evolution: set G__USEBYTECOUNT flag
+//        *   nomap: (ignored by roocling; prevents entry in ROOT's rootmap file)
+//        *   stub: (ignored by rootcling was a directly for CINT code generation)
+//        *   version(x): sets the version number of the class to x
+//        */
